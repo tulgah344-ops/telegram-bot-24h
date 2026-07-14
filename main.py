2178bb5cfa3a112fe51a0133425b3e087e87f86d
tulgah344-ops <tulgah344@gmail.com>
2026-07-14 11:30:25 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from telegram.ext import Application, CommandHandler
+import os
+from flask import Flask
+import threading
 
 BOT_TOKEN = "8749944205:AAFV3JEJnF6ZomVOEgb1C1_9Sm9iB18htzE"
+
+app_flask = Flask('')
+
+@app_flask.route('/')
+def home():
+    return "Bot is alive"
 
 async def start(update, context):
     await update.message.reply_text("سلام ربات روشنه ✅")
 
-app = Application.builder().token(BOT_TOKEN).build()
-app.add_handler(CommandHandler("start", start))
+application = Application.builder().token(BOT_TOKEN).build()
+application.add_handler(CommandHandler("start", start))
 
-app.run_polling()
+def run_bot():
+    application.run_polling()
+
+threading.Thread(target=run_bot).start()
+
+if __name__ == '__main__':
+    app_flask.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
