0f6513ac9f386e7052d66e5185899df1b71abe98
tulgah344-ops <tulgah344@gmail.com>
2026-07-14 09:58:47 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,8 @@ TOKEN = os.getenv("TOKEN")
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام! رباتت آنلاینه ✅")
 
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
-
-app.run_polling()
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    print("Bot is running...")
+    app.run_polling()
